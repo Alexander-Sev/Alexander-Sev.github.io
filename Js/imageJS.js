93f746e14d5f8f9512a29fdedf484338611ab159
Alexander-Sevastyanov <148810139+Alexander-Sev@users.noreply.github.com>
2025-11-02 19:24:37 -0500

    Update imageJS.js

--- Js/imageJS.js
+++ Js/imageJS.js
@@ -1,1 +1,23 @@
 
+function setImages(){
+    if (document.URL){ //if the URL is something then allow it to run, need to change this code
+    //change Background image
+    document.body.style.backgroundImage = "url('images/pexels-markusspiske-1089438.jpg')";
+
+    //change the background image for the login blob
+    //document.getElementById("mainHandler").style.backgroundImage = "url('static/images/husky_qa.jpg')";
+
+    //change icon image
+    //var link = document.createElement('link');
+    //link.type = 'image/x-icon';
+    //link.rel = 'shortcut icon';
+    //link.href = "static/images/Icon/johnathan.ico";
+    //document.getElementsByTagName('head')[0].appendChild(link);
+    
+    }
+}
+
+
+setImages();
+
+// Ezequiel Valencia, 9/4/2022, Changed the image locations so that they are better structured./home/cse/Lab4/webapp/static/images
